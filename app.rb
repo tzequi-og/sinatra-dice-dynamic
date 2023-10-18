1abff9b7ba085476df9c23cc56698e03411a9413
tzequi-og <146213960+tzequi-og@users.noreply.github.com>
2023-10-18 14:03:31 +0000

    dynamic

--- app.rb
+++ app.rb
@@ -52,3 +52,18 @@ get("/dice/5/4") do
 
   erb(:five_four)
 end
+
+get ("/dice/:dice/:sides") do
+  @dice_number = params.fetch("dice").to_i
+  @dice_sides = params.fetch("sides").to_i
+  
+  @rolls = []
+
+  @dice_number.times do
+    die = rand(1..@dice_sides)
+
+    @rolls.push(die)
+  end
+
+  erb(:flexible)
+end
